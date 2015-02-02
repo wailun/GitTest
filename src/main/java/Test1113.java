6c1657be9d02f939e18373d7395047ff3ff2b0aa
wm <wangmengwangyi@163.com>
2015-02-02 17:19:58 +0800

    println nowTime

--- src/main/java/Test1113.java
+++ src/main/java/Test1113.java
@@ -3,6 +3,8 @@ import com.wm.pj01.AA;
 import org.apache.poi.poifs.crypt.HashAlgorithm;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by wm on 14-11-13.
@@ -13,6 +15,7 @@ public class Test1113 {
         String str="恭喜你,    抢到5元    红包1个";
         String []strings=str.split("\\s");
         System.out.println(JsonUtils.toStr(strings));
+        System.out.println(Calendar.getInstance().getTime());
 
     }
 
